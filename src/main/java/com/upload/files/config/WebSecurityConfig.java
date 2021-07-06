fb091e81d16deed547715024a117a9c021ee67ce
Minji-Kim-11 <vivor1993@gmail.com>
2021-07-07 04:19:09 +0900

    민지추가
    테이블 생성
    role(권한)
    user
    user_role(mapping)
    
    -Java-
    config - WebSecurityConfig
    controller - AccountController
    model - Role, User
    service - UserService
    repository -userRepository
    
    -css-
    signin, starter-template
    
    -html-
    account - login, register
    fragments - common

--- src/main/java/com/upload/files/config/WebSecurityConfig.java
+++ src/main/java/com/upload/files/config/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
+
         return new BCryptPasswordEncoder();
     }
 }
