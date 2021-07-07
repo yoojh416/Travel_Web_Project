d2fde9876f7eb9b0009c987c8274899cefb7cf3e
ryu-jihye <dinar66@naver.com>
2021-07-08 03:21:17 +0900

    지혜 header.html : 관리자 링크 추가(href) home.html : board/list 링크 추가(href)

--- src/main/java/com/upload/files/controller/HomeController.java
+++ src/main/java/com/upload/files/controller/HomeController.java
@@ -1,38 +1,16 @@
 package com.upload.files.controller;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@SpringBootApplication(scanBasePackages = {"com.upload.files"})
 public class HomeController {
-    @GetMapping("hello")
-    public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "hello";
-    }
-
-
-    @GetMapping("default")
-    public String Idefault(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "layout/default";
-    }
-
-
-    @GetMapping("header")
-    public String header(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "/fragments/header";
-    }
-
-    @GetMapping("footer")
-    public String footer(Model model) {
-        model.addAttribute("data", "hello!!");
-        return "/fragments/footer";
-    }
 
     @GetMapping("/")
     public String home(Model model) {
@@ -41,11 +19,11 @@ public class HomeController {
     }
 
     //유저(user)
-//    @GetMapping(value = "account/login")
-//    public String login(Model model) {
-//        model.addAttribute("data", "login!!");
-//        return "/account/login";
-//    }
+    @GetMapping(value = "user/login")
+    public String login(Model model) {
+        model.addAttribute("data", "login!!");
+        return "/user/login";
+    }
 
     @GetMapping(value = "user/logout")
     public String logout(Model model) {
@@ -65,23 +43,11 @@ public class HomeController {
         return "/user/userInfo";
     }
 
-    /*관리자*/
-    @GetMapping(value = "admin/upload")
-    public String itemUpload(Model model) {
-        model.addAttribute("data", "itemUpload!!");
-        return "/admin/upload";
-    }
-
     /*여행지, 계절, 테마 검색*/
-    @GetMapping(value = "list")
+    @RequestMapping("list")
     public String boardList(Model model) {
-        model.addAttribute("data", "boardList!!");
+        //model.addAttribute("data", "boardList!!");
         return "/board/list";
     }
-    /*이미지 클릭 시 상세페이지 이동*/
-    @GetMapping(value = "get")
-    public String boardGet(Model model) {
-        model.addAttribute("data", "boardGet!!");
-        return "board/get";
-    }
+
 }
