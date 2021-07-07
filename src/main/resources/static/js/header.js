1132c0bc80969a8c7d6aa2252bce0d2423234fe6
ryu-jihye <dinar66@naver.com>
2021-07-07 11:33:25 +0900

    지혜 home.html : main 화면 header.html : header footer.html : footer default.html : content만 추가 시 header, footer 자동 생성하는 틀
    properties.yml :jpa 이후 변경사항 있음, 기존 jpa, logging #표시

---
+++ src/main/resources/static/js/header.js
@@ -0,0 +1,46 @@
+const toogleBtn = document.querySelector('.navbar_toogleBtn');
+const menu = document.querySelector('.navbar_menu');
+const icons = document.querySelector('.navbar_icons');
+
+toogleBtn.addEventListener('click', () => {
+	menu.classList.toggle('active');
+	icons.classList.toggle('active');
+
+
+$(function () {
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5; // 이벤트를 발생시킬 스크롤의 이동 범위
+    var navbarHeight = $("header").outerHeight();
+
+    $(window).scroll(function(event){
+        didScroll = true;
+    });
+
+    setInterval(function() {
+        if (didScroll) {
+            hasScrolled();
+            didScroll = false;
+        }
+    }, 250); // 스크롤이 멈춘 후 동작이 실행되기 까지의 딜레이
+
+    function hasScrolled() {
+        var st = $(this).scrollTop(); // 현재 window의 scrollTop 값
+
+        // delta로 설정한 값보다 많이 스크롤 되어야 실행된다.
+        if(Math.abs(lastScrollTop - st) <= delta)
+            return;
+
+        if (st > lastScrollTop && st > navbarHeight){
+            // 스크롤을 내렸을 때
+            $("header").slideUp("fast"); // header 숨기기
+        } else {
+            // 스크롤을 올렸을 때
+            if(st + $(window).height() < $(document).height()) {
+                $("header").slideDown("fast"); // header 보이기
+            }
+        }
+
+        lastScrollTop = st; // 현재 멈춘 위치를 기준점으로 재설정
+    }
+})
