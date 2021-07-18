a5aa14d7cfea03ee4ad27348add05d44ef57c279
Hannah <gkssk8827@gmail.com>
2021-07-19 04:12:18 +0900

    정한나(7/19) 베스트 리뷰 게시판 개설 로직 : 리뷰 게시판에서 베스트 리뷰 게시판으로 이동시킴 -> 리뷰 게시판 게시글은 자동 삭제 -> 수정이 필요없으므로 read/delete 만 구현

---
+++ src/main/java/com/upload/files/controller/BestReviewController.java
@@ -0,0 +1,89 @@
+package com.upload.files.controller;
+
+
+import com.upload.files.entity.Article;
+import com.upload.files.entity.BestReview;
+import com.upload.files.repository.ArticleRepository;
+import com.upload.files.repository.BestReviewRepository;
+import com.upload.files.service.ArticleService;
+import com.upload.files.service.BestReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
+@Controller
+public class BestReviewController { //리뷰용 컨트롤러
+
+	@Autowired private BestReviewRepository bestReviewRepository;
+	@Autowired private BestReviewService bestReviewService;
+	@Autowired private ArticleRepository articleRepository;
+	@Autowired private ArticleService articleService;
+
+	/** 베스트 게시판으로 옮기기 */
+	@GetMapping("/transfer/{id}")
+	public String setArticle(BestReview bestReview
+			, Model model
+			, @PathVariable Long id) {
+
+		Article article = articleRepository.findById(id).get();
+
+		bestReview.setTitle(article.getTitle());
+		bestReview.setContent(article.getContent());
+		bestReview.setWriter(article.getWriter());
+		bestReview.setRegisterDate(article.getRegisterDate());
+
+		bestReviewRepository.save(bestReview);
+
+		List<BestReview> bestReviews = bestReviewRepository.findAll();
+		model.addAttribute("bestReviews", bestReviews);
+
+		/* 리뷰 게시판에 있는 것 삭제 */
+		articleRepository.deleteById(id);
+
+		return "redirect:/bestReview/list";
+	}
+
+	/** 전체 리스트 + 페이징 */
+	@GetMapping("/bestReview/list")
+	public String getBestReviewList(Model model, @PageableDefault Pageable pageable
+			, @RequestParam(value = "page", defaultValue = "1") String pageNum) {
+		Page<BestReview> bestReviews = bestReviewService.getBestReviewList(pageable);
+		model.addAttribute("bestReviews", bestReviews);
+
+		return "bestReview/list";
+	}
+
+	/**상세 페이지*/
+	@GetMapping("/bestReview/{id}")
+	public String getArticle(@PathVariable("id") Long id, Model model) {
+		BestReview bestReview = bestReviewRepository.findById(id).get();
+		model.addAttribute("bestReview", bestReview);
+
+		List<BestReview> bestReviews = bestReviewRepository.findAll();
+		model.addAttribute("bestReviews", bestReviews);
+
+		return "bestReview/detail";
+	}
+
+	/** 삭제하기*/
+	@GetMapping("/bestReview/delete/{id}")
+	@Transactional
+	public String deleteArticle(Model model, @PathVariable Long id) {
+		bestReviewRepository.deleteById(id);
+
+		return "redirect:/bestReview/list";
+	}
+
+}
---
+++ src/main/java/com/upload/files/entity/BestReview.java
@@ -0,0 +1,31 @@
+package com.upload.files.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@ToString
+@Table(name = "best_review")
+public class BestReview { //댓글 테이블 생성용
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    @Column(length= 100000000)
+    private String content;
+
+    private String writer;
+
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd", timezone="Asia/Seoul")
+    private LocalDateTime registerDate;
+
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd", timezone="Asia/Seoul")
+    private LocalDateTime updateDate;
+}
