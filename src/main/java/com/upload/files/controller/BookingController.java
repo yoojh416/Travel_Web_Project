60d65dd4fec063298cb228a25a3462b574ab04e5
yoojh416 <yoojh416@gmail.com>
2021-07-21 22:05:26 +0900

    지형(7월 21일) : article 로직 수정   security token 저장
    ** Home.html 내 아이템 사진 출력 기능 구현 필요함

--- src/main/java/com/upload/files/controller/BookingController.java
+++ src/main/java/com/upload/files/controller/BookingController.java
@@ -1,7 +1,9 @@
 package com.upload.files.controller;
 
 import com.upload.files.entity.Booking;
+import com.upload.files.entity.Product;
 import com.upload.files.repository.BookingRepository;
+import com.upload.files.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 @RequestMapping("/order")
@@ -18,18 +22,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BookingController {
 
     @Autowired private BookingRepository bookingRepository;
+    @Autowired private ProductService productService;
 
     /** 예약페이지로 이동 */
-    @GetMapping("/book")
-    public String main(Model model) {
+    @GetMapping("/book/{proNo}")
+    public String main(Model model, @PathVariable Long proNo) {
         model.addAttribute("order", new Booking());
+
+        Product product = productService.findOne(proNo);
+        model.addAttribute("product", product);
 
         return "order/booking";
     }
 
     /** 예약된 내용 저장해서 확인 페이지로 보냄 */
     @PostMapping("/confirm")
-    public String save(Booking booking, Model model) {
+    public String save(Booking booking, HttpServletRequest request, Model model) {
+        booking.setOrderPrice(Integer.parseInt(request.getParameter("OrderPrice")));
         bookingRepository.save(booking);
         model.addAttribute("order", booking);
 
